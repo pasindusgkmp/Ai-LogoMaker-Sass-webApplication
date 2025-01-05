c1bd88d58a581edbd5456e5fb026193bdf750aba
Pasidu Sathsara <pasindugunasekara889@gmail.com>
2025-01-06 01:38:55 +0530

    project setup done

---
+++ ai-logo-generator/app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>AI Logo Generator</h1>
+      <Button>jjjhf</Button>
+    </div>
+  );
+}
