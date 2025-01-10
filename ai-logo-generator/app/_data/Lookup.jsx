9880ce09be7e2d0309d46404bbf51fb74e533a36
Pasidu Sathsara <pasindugunasekara889@gmail.com>
2025-01-10 20:40:44 +0530

    landing screen done

---
+++ ai-logo-generator/app/_data/Lookup.jsx
@@ -0,0 +1,6 @@
+export default{
+    HeroHeading : 'Ai Logo Maker',
+    HeroSubHeading : 'Perfect Logoes for Apps,Businesses,and Websites',
+    HeroDesc: 'Craft unique and professional logos effortlessly with our AI-Powered tool perfect for apps,businesses,and websites,and more.',
+    HeroInputPlaceholder : 'Enter your logo name'
+}
--- ai-logo-generator/app/page.js
+++ ai-logo-generator/app/page.js
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import Hero from './_components/Hero'
 
 export default function Home() {
   return (
-    <div>
-      <h1>AI Logo Generator</h1>
-      <Button>jjjhf</Button>
-    </div>
-  );
+    <main>
+      <Hero />
+    </main>
+  )
 }
